c1f257a8bea23686e2c0aa4c3a4e0cc9478faaa0
Faisal ahmed <faisal2410@yahoo.com>
2022-10-21 19:07:54 +0600

    initial commit

---
+++ __note.js
@@ -0,0 +1,23 @@
+/**
+ * Difference between a good developer and a great developer is simply know 
+how to do something and how something is done/works
+
+Hotel example: what is an api
+
+https://www.youtube.com/watch?v=s7wmiS2mSXY
+
+First website :
+info.cern.ch
+
+
+A journey of a thousand miles begins with a single step
+
+curl -v www.google.com
+ * 
+ * 
+ * */ 
+
+/**
+ * HTTP- start line , header, body
+ * 
+ * */ 
